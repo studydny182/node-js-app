cdbfe7237c9753ed6e0a665a145702bdff87f22c
OPI SOPIAH <opisopiah@MacBook-Air-OPI.local>
2025-07-10 18:31:12 +0700

    Update konfigurasi koneksi database

--- db.js
+++ db.js
@@ -2,9 +2,9 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'passwordkamu', // ganti sesuai konfigurasi kamu
+  host: 'mycruddb.ctsaao2iigig.ap-southeast-3.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Vic123tory!', // ganti sesuai konfigurasi kamu
   database: 'crud_app'
 });
 
